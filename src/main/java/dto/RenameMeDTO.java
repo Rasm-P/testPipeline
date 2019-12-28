3c344bce905aaef4d2341df3ef969afd505ef276
Rasm-P <rasmus.pra@gmail.com>
2019-12-28 15:30:18 +0100

    a

---
+++ src/main/java/dto/RenameMeDTO.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dto;
+
+import entities.RenameMe;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public class RenameMeDTO {
+    
+    @Schema(required = true, example = "String1")
+    private String dummyStr1;
+    @Schema(required = true, example = "String2")
+    private String dummyStr2;
+    
+    public RenameMeDTO(RenameMe renameMe) {
+        this.dummyStr1 = renameMe.getDummyStr1();
+        this.dummyStr2 = renameMe.getDummyStr2();
+    }
+
+    public String getDummyStr1() {
+        return dummyStr1;
+    }
+
+    public void setDummyStr1(String dummyStr1) {
+        this.dummyStr1 = dummyStr1;
+    }
+
+    public String getDummyStr2() {
+        return dummyStr2;
+    }
+
+    public void setDummyStr2(String dummyStr2) {
+        this.dummyStr2 = dummyStr2;
+    }
+    
+}
