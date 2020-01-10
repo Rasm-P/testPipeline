262d3ae845d7af2074967e9db08123fc938dec41
Rasm-P <rasmus.pra@gmail.com>
2020-01-10 19:10:21 +0100

    a

--- src/test/java/utils/SettingsTest.java
+++ src/test/java/utils/SettingsTest.java
@@ -13,7 +13,7 @@ public class SettingsTest {
     @Test
     public void testGetDEV_DBConnection(){
         //This test is a little bit hacky since it uses a property that can be changed by users
-        //Is Better than NO test
+        //Is Better than NO test.
         String dbName = Settings.getPropertyValue("db.database");
         assertEquals(String.format("jdbc:mysql://localhost:3307/%s",dbName), Settings.getDEV_DBConnection());
     }
@@ -21,7 +21,7 @@ public class SettingsTest {
     @Test
     public void testGetTEST_DBConnection(){
         //This test is also a little bit hacky since it uses a property that can be changed by users
-        //Is Better than NO test
+        //Is Better than NO test.
         String dbName = Settings.getPropertyValue("dbtest.database");
         assertEquals(String.format("jdbc:mysql://localhost:3307/%s",dbName), Settings.getTEST_DBConnection());
     }
